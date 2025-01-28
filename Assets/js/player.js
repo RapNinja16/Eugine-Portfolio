b224af514a7bbfa440d8f6cdb502f0b73ab70e51
RapNinja16 <eujhindepp@gmail.com>
2025-01-28 09:52:33 +0800

    VideoUpdate3

--- Assets/js/player.js
+++ Assets/js/player.js
@@ -1,63 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const videoWrappers = document.querySelectorAll(".video-wrapper");
-  
-    videoWrappers.forEach((wrapper) => {
-      const video = wrapper.querySelector("video");
-      const playButton = wrapper.querySelector(".play-button");
-      const timeline = wrapper.querySelector(".timeline");
-  
-      video.addEventListener("click", () => {
-        if (video.paused) {
-          video.play();
-          playButton.classList.add("hide");
+  const videoWrappers = document.querySelectorAll(".video-wrapper");
 
-          
-        } else {
-          video.pause();
-          playButton.classList.remove("hide");
+  videoWrappers.forEach((wrapper) => {
+    const video = wrapper.querySelector("video");
+    const playButton = wrapper.querySelector(".play-button");
+    const timeline = wrapper.querySelector(".timeline");
+
+    const updateTimeline = () => {
+      timeline.max = video.duration;
+      timeline.value = video.currentTime;
+    };
+
+    const pauseAllVideos = () => {
+      document.querySelectorAll(".video").forEach((otherVideo) => {
+        if (otherVideo !== video) {
+          otherVideo.pause();
+          const otherPlayButton = otherVideo
+            .closest(".video-wrapper")
+            .querySelector(".play-button");
+          otherPlayButton.classList.remove("hide");
         }
       });
-  
-      
-      playButton.addEventListener("click", () => {
-        if (video.paused) {
-          video.play();
-          playButton.classList.add("hide");
-           
-            document.querySelectorAll(".video").forEach((otherVideo) => {
-                if (otherVideo !== video) {
-                otherVideo.pause();
-                const otherPlayButton = otherVideo
-                    .closest(".video-wrapper")
-                    .querySelector(".play-button");
-                otherPlayButton.classList.remove("hide");
-                playButton.classList.add("hide");
-                }
-            });
-          
-          
-          
-           
-            video.addEventListener("timeupdate", () => {
-                timeline.value = video.currentTime;
-            });
-        
-          
-            video.addEventListener("loadedmetadata", () => {
-                timeline.max = video.duration;
-            });
-        
-            
-            timeline.addEventListener("input", () => {
-                video.currentTime = timeline.value;
-            });
-        } else {
+    };
+
+    video.addEventListener("click", () => {
+      if (video.paused) {
+        video.play();
+        playButton.classList.add("hide");
+      } else {
+        video.pause();
         playButton.classList.remove("hide");
-          video.pause();
-          
-          
-        }
-      });
+      }
+    });
+
+    playButton.addEventListener("click", () => {
+      if (video.paused) {
+        pauseAllVideos();
+        video.play();
+        playButton.classList.add("hide");
+      } else {
+        video.pause();
+        playButton.classList.remove("hide");
+      }
+    });
+
+    video.addEventListener("timeupdate", () => {
+      timeline.value = video.currentTime;
+    });
+
+    video.addEventListener("loadedmetadata", updateTimeline);
+
+    timeline.addEventListener("input", () => {
+      video.currentTime = timeline.value;
     });
   });
-  
+});
